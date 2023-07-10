a4df4609ffc53765256f4a6b172a27f0ebafd604
david30-maker <davidokpah396@gmail.com>
2023-07-10 16:41:01 +0100

    fix linter error

--- db/migrate/20230710140920_create_recipe_foods.rb
+++ db/migrate/20230710140920_create_recipe_foods.rb
@@ -1,3 +1,7 @@
+# frozen_string_literal: true
+
+# Migration to create the RecipeFoods table.
+
 class CreateRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     create_table :recipe_foods do |t|
