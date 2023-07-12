3660342f0de79f0c47a6a605ab524c42c070308a
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-07-12 16:04:34 +0530

    Fix:rubcop error

--- app/controllers/shopping_list_controller.rb
+++ app/controllers/shopping_list_controller.rb
@@ -1,19 +1,19 @@
 class ShoppingListController < ApplicationController
-    def index
-      recipe_foods = []
-  
-      current_user.recipes.each do |recipe|
-        recipe.foods.each do |food|
-          recipe_foods << food unless recipe_foods.include? food
-        end
+  def index
+    recipe_foods = []
+
+    current_user.recipes.each do |recipe|
+      recipe.foods.each do |food|
+        recipe_foods << food unless recipe_foods.include? food
       end
-  
-      @list = []
-  
-      current_user.foods.each do |food|
-        @list << food unless recipe_foods.include? food
-      end
-  
-      @total_food_value = @list.sum(&:price)
     end
+
+    @list = []
+
+    current_user.foods.each do |food|
+      @list << food unless recipe_foods.include? food
+    end
+
+    @total_food_value = @list.sum(&:price)
   end
+end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,11 +1,10 @@
 class Recipe < ApplicationRecord
-    belongs_to :user, class_name: 'User', foreign_key: 'user_id'
+  belongs_to :user, class_name: 'User', foreign_key: 'user_id'
 
-  
-    has_and_belongs_to_many :foods, join_table: :foods_recipes
-  
-    validates :name, presence: true
-    validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
-    validates :description, presence: true
+  has_and_belongs_to_many :foods, join_table: :foods_recipes
+
+  validates :name, presence: true
+  validates :preparation_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :cooking_time, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :description, presence: true
 end
