17c09f487ff7c022338b6733afba0ba671195d22
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-07-12 18:27:09 +0530

    add functionalities for recipes controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-<<<<<<< HEAD
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -8,10 +7,4 @@ class User < ApplicationRecord
   has_many :recipes, class_name: 'Recipe', foreign_key: 'user_id'
 
   validates :name, presence: true
-=======
-    has_secure_password
-    has_many :recipes
-    has_many :foods
-    validates :username, presence: true, uniqueness: true
->>>>>>> de2c864 (add functionalities for foods controller)
 end
