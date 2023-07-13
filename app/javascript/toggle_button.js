756df41612fb34c30092c0add99c14198ccea337
david30-maker <davidokpah396@gmail.com>
2023-07-13 04:46:10 +0100

    add functionalitie to toggle button

---
+++ app/javascript/toggle_button.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const visibilitySwitch = document.querySelector('.visibility-switch');
+  
+    visibilitySwitch.addEventListener('click', function() {
+      const form = visibilitySwitch.closest('form');
+      const publicField = form.querySelector('[name="recipe[public]"]');
+      const isActive = visibilitySwitch.classList.contains('active');
+  
+      visibilitySwitch.classList.toggle('active');
+      publicField.value = isActive ? 'true' : 'false';
+  
+      // Additional logic to submit the form if needed
+      form.submit();
+    });
+  });
+  
