57e6baff9aea89ec7806bc0d22844f06f68ad5fe
Santosh-Konappanavar <siddukundaragi20@gmail.com>
2023-07-13 21:39:40 +0530

    rubocop error

--- db/migrate/20230712133734_change_column_name_to_text.rb
+++ db/migrate/20230712133734_change_column_name_to_text.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class ChangeColumnNameToText < ActiveRecord::Migration[7.0]
   def change
     change_column :recipes, :description, :text
--- db/migrate/20230712135829_rename_columnsin_recipe_foods.rb
+++ db/migrate/20230712135829_rename_columnsin_recipe_foods.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class RenameColumnsinRecipeFoods < ActiveRecord::Migration[7.0]
   def change
     rename_column :recipe_foods, :foods_id, :food_id
