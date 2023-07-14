67bb3f4cdca60ce1ff46ab226b4d76b6ea5eb5aa
david30-maker <davidokpah396@gmail.com>
2023-07-14 18:23:16 +0100

    fix linter error

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,29 +1,28 @@
 class IngredientsController < ApplicationController
+  def index
+    @ingredients = Ingredient.all
+  end
 
-    def index
-        @ingredients = Ingredient.all
+  # def show
+  #     @ingredient = Ingredient.find(params[:id])
+  # end
+
+  def new
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to ingredients_path, notice: 'Ingredient created successfully.'
+    else
+      render :new
     end
+  end
 
-    # def show
-    #     @ingredient = Ingredient.find(params[:id])
-    # end
+  private
 
-    def new
-        @ingredient = Ingredient.new
-    end
-    
-    def create
-        @ingredient = Ingredient.new(ingredient_params)
-        if @ingredient.save
-            redirect_to ingredients_path, notice: 'Ingredient created successfully.'
-        else
-            render :new
-        end
-    end
-
-    private
-
-    def ingredient_params
-        params.require(:ingredient).permit(:name, :description, :category, :measurement_unit)
-    end
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :description, :category, :measurement_unit)
+  end
 end
